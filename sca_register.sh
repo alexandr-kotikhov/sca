d7dfc1b9bd9f62d949f4f5e7fc2f655fb7557e64
Kotikhov Alexandr <alexandr.kotikhov@waveaccess.ru>
2020-10-28 21:54:34 +0300

    some improvement 2

--- sca_register.sh
+++ sca_register.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare SCA_DIR=$(cd "$(dirname $0)"; pwd)
+declare SCA_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
 function _sca_completion() {
   wordlist=$("${SCA_DIR}/sca_runner.sh" COMPLETION "$COMP_CWORD" "${COMP_WORDS[*]}")
